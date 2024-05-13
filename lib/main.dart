246dfa126822a2f8b1dd3e05c5f94b9ff57ec415
Aayush <aayushdc123@yahoo.com>
2024-05-13 11:21:58 +0545

    UI Updated

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'themes/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'models/my_theme_provider.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+        create: (context) => MyThemeModel(),
+        child: Consumer<MyThemeModel>(
+          builder: (context, theme, child) => MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Analog Clock',
+            theme: themeData(context),
+            darkTheme: darkThemeData(context),
+            themeMode: theme.isLightTheme ? ThemeMode.light : ThemeMode.dark,
+            home: const HomeScreen(),
+          ),
+        ));
+  }
+}
